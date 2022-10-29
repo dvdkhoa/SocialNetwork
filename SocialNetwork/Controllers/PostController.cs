56573e067d3a0ad40117d6523be97a615b54b894
Đăng Khoa Đặng Văn <khoa028@gmail.com>
2022-10-29 20:25:10 +0700

    ...

--- SocialNetwork/Controllers/PostController.cs
+++ SocialNetwork/Controllers/PostController.cs
@@ -8,6 +8,7 @@ using SocialNetwork.BLL.Services.Implements;
 using SocialNetwork.DTO.Entities;
 using SocialNetwork.DTO.Posts;
 using SocialNetwork.DTO.Shared;
+using System.Collections;
 
 namespace SocialNetwork.Api.Controllers
 {
@@ -178,5 +179,134 @@ namespace SocialNetwork.Api.Controllers
             await _postService.DeletePostAsync(postId);
             return Ok();
         }
+
+
+        private async Task<String> uploadVideo(IFormFile file)
+        {
+            // Get the server path, wwwroot
+            string webRootPath = _env.WebRootPath;
+
+            // Building the path to the uploads directory
+            var fileRoute = Path.Combine(webRootPath, "uploads");
+
+            // Get the mime type
+            var mimeType = file.ContentType;
+            //var mimeType = HttpContext.Request.Form.Files.GetFile("file").ContentType;
+
+            // Get File Extension
+            string extension = System.IO.Path.GetExtension(file.FileName);
+
+            // Generate Random name.
+            string name = Guid.NewGuid().ToString().Substring(0, 8) + extension;
+
+            // Build the full path inclunding the file name
+            string link = Path.Combine(fileRoute, name);
+
+            // Create directory if it dose not exist.
+            FileInfo dir = new FileInfo(fileRoute);
+            dir?.Directory?.Create();
+
+            // Basic validation on mime types and file extension
+            string[] videoMimetypes = { "video/mp4", "video/webm", "video/ogg" };
+            string[] videoExt = { ".mp4", ".webm", ".ogg" };
+
+            try
+            {
+                if (Array.IndexOf(videoMimetypes, mimeType) >= 0 && (Array.IndexOf(videoExt, extension) >= 0))
+                {
+                    // Copy contents to memory stream.
+                    Stream stream;
+                    stream = new MemoryStream();
+                    file.CopyTo(stream);
+                    stream.Position = 0;
+                    String serverPath = link;
+
+                    // Save the file
+                    using (FileStream writerFileStream = System.IO.File.Create(serverPath))
+                    {
+                        await stream.CopyToAsync(writerFileStream);
+                        writerFileStream.Dispose();
+                    }
+
+                    // Return the file path as json
+                    Hashtable videoUrl = new Hashtable();
+                    videoUrl.Add("link", "/uploads/" + name);
+                    return videoUrl.ToJson().ToString();
+                }
+                throw new ArgumentException("The video did not pass the validation");
+            } catch(ArgumentException ex) {
+                Console.WriteLine(ex.Message);
+                return "";
+            }
+        }
+
+        [HttpPost("UploadFiles")]
+        [Produces("application/json")]
+        public async Task<IActionResult> Post(List<IFormFile> files)
+        {
+            // Get the file from the POST request
+            //var theFile = HttpContext.Request.Form.Files.GetFile("file");
+            var theFile = files[0];
+
+            // Get the server path, wwwroot
+            string webRootPath = _env.WebRootPath;
+
+            // Building the path to the uploads directory
+            var fileRoute = Path.Combine(webRootPath, "uploads");
+
+            // Get the mime type
+            var mimeType = files[0].ContentType;
+            //var mimeType = HttpContext.Request.Form.Files.GetFile("file").ContentType;
+
+            // Get File Extension
+            string extension = System.IO.Path.GetExtension(theFile.FileName);
+
+            // Generate Random name.
+            string name = Guid.NewGuid().ToString().Substring(0, 8) + extension;
+
+            // Build the full path inclunding the file name
+            string link = Path.Combine(fileRoute, name);
+
+            // Create directory if it dose not exist.
+            FileInfo dir = new FileInfo(fileRoute);
+            dir.Directory.Create();
+
+            // Basic validation on mime types and file extension
+            string[] videoMimetypes = { "video/mp4", "video/webm", "video/ogg" };
+            string[] videoExt = { ".mp4", ".webm", ".ogg" };
+
+            try
+            {
+                if (Array.IndexOf(videoMimetypes, mimeType) >= 0 && (Array.IndexOf(videoExt, extension) >= 0))
+                {
+                    // Copy contents to memory stream.
+                    Stream stream;
+                    stream = new MemoryStream();
+                    theFile.CopyTo(stream);
+                    stream.Position = 0;
+                    String serverPath = link;
+
+                    // Save the file
+                    using (FileStream writerFileStream = System.IO.File.Create(serverPath))
+                    {
+                        await stream.CopyToAsync(writerFileStream);
+                        writerFileStream.Dispose();
+                    }
+
+                    // Return the file path as json
+                    Hashtable videoUrl = new Hashtable();
+                    videoUrl.Add("link", "/uploads/" + name);
+
+                    return Ok(videoUrl);
+                }
+                throw new ArgumentException("The video did not pass the validation");
+            }
+
+            catch (ArgumentException ex)
+            {
+                return Ok(ex.Message);
+            }
+        }
     }
 }
+
